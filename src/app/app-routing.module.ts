551be156c63f41504ce4b0532216ea31193ced17
André <ni.mailix@gmail.com>
2023-08-03 22:12:56 +0300

    03/08/2023 22:12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
-const routes: Routes = [];
+
+  const routes: Routes = [
+    { path: '', redirectTo: '/authentification', pathMatch: 'full' }, // Redirige la racine vers la page d'authentification
+    { path: 'authentification', component: AuthentificationComponent },
+    // ... autres routes de votre application ...
+  ];
+  
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
