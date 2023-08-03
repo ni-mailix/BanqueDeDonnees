05d2471090977006d27e33828556dcf77e82e862
André <ni.mailix@gmail.com>
2023-08-03 23:56:49 +0300

    commit 03/08/2023 23:56

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +29,10 @@ import { AuthentificationService } from './services/authentification.service'; /
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
     AppRoutingModule,
     FormsModule
   ],
